8aef9626f6f9eb92a8ad13376c912a7fbfb04626
Alexey Bobrikov <a.beaverg@gmail.com>
2023-04-05 02:23:59 +0300

    update

--- src/test/java/page/VirtKeyHomePage.java
+++ src/test/java/page/VirtKeyHomePage.java
@@ -2,10 +2,7 @@ package page;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import waits.ExplicitCondition;
@@ -40,31 +37,86 @@ public class VirtKeyHomePage extends page.AbstractPage {
         return this;
     }
 
+    public VirtKeyHomePage maximizeTextArea() {
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("document.getElementsByClassName('use-keyboard-input')[0].style.width = '100%';");
+        executor.executeScript("document.getElementsByClassName('use-keyboard-input')[0]" +
+                ".style.height = '450px';");
+        executor.executeScript("document.getElementsByClassName('use-keyboard-input')[0]" +
+                ".style.background = 'rgb(254, 255, 204)';");
+        return this;
+    }
+
     public String writeText(String str) {
         StringBuilder text = new StringBuilder();
         String keyLetter;
         boolean isShift = false;
-        HashMap<Character, Character> shiftMap = createEnglishShiftMap();
+        boolean isRussian = false;
+        HashMap<Character, Character> EnShiftMap = createEnglishShiftMap();
+        HashMap<Character, Character> RuShiftMap = createRussianShiftMap();
+        HashMap<Character, Character> EnRuMap = createEnRuMap();
 
         for (char letter : str.toCharArray()) {
             if (letter == ' ') {
                 keyLetter = "space";
-            } else if (shiftMap.containsKey(letter)) {
+            } else if (letter == '\n') {
+                keyLetter = "enter";
+            } else if (letter == '№') {
                 if (!isShift) {
                     clickShiftWithJSExecutor();
                     isShift = true;
                 }
-                keyLetter = String.valueOf(shiftMap.get(letter));
+                if (!isRussian) {
+                    clickLangWIthJSExecutor();
+                    isRussian = true;
+                }
+                keyLetter = "3";
+            } else if (EnShiftMap.containsKey(letter)) {
+                if (!isShift) {
+                    clickShiftWithJSExecutor();
+                    isShift = true;
+                }
+                if (isRussian) {
+                    clickLangWIthJSExecutor();
+                    isRussian = false;
+                }
+                keyLetter = String.valueOf(EnShiftMap.get(letter));
+            } else if (RuShiftMap.containsKey(letter)) {
+                if (!isShift) {
+                    clickShiftWithJSExecutor();
+                    isShift = true;
+                }
+                if (!isRussian) {
+                    clickLangWIthJSExecutor();
+                    isRussian = true;
+                }
+                keyLetter = String.valueOf(EnRuMap.get(RuShiftMap.get(letter)));
             } else {
                 if (isShift) {
                     clickShiftWithJSExecutor();
                     isShift = false;
                 }
-                keyLetter = String.valueOf(letter);
+                if (EnShiftMap.containsValue(letter)) {
+                    if (isRussian) {
+                        clickLangWIthJSExecutor();
+                        isRussian = false;
+                    }
+                    keyLetter = String.valueOf(letter);
+                } else {
+                    if (!isRussian) {
+                        clickLangWIthJSExecutor();
+                        isRussian = true;
+                    }
+                    keyLetter = String.valueOf(EnRuMap.get(letter));
+                }
             }
-            WebElement key = driver.findElement(By.id("keyboard__" + keyLetter));
-            new ExplicitCondition(driver, key);
-            key.click();
+            try {
+                WebElement key = driver.findElement(By.id("keyboard__" + keyLetter));
+                new ExplicitCondition(driver, key);
+                key.click();
+            } catch (ElementClickInterceptedException e) {
+                clickWIthJSExecutor("keyboard__" + keyLetter);
+            }
             text.append(letter);
         }
 
@@ -78,6 +130,17 @@ public class VirtKeyHomePage extends page.AbstractPage {
         executor.executeScript("document.getElementById('keyboard__shift').click();");
     }
 
+    private void clickLangWIthJSExecutor() {
+        logger.info("Was clicked key 'Language'");
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("document.getElementById('keyboard__EN').click();");
+    }
+
+    private void clickWIthJSExecutor(String id) {
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("document.getElementById('" + id + "').click();");
+    }
+
     private HashMap<Character, Character> createEnglishShiftMap() {
         logger.info("Was created english Shift Map");
         return new HashMap<>() {{
@@ -88,6 +151,28 @@ public class VirtKeyHomePage extends page.AbstractPage {
             put('P', 'p'); put('A', 'a'); put('S', 's'); put('D', 'd'); put('F', 'f'); put('G', 'g'); put('H', 'h');
             put('J', 'j'); put('K', 'k'); put('L', 'l'); put('Z', 'z'); put('X', 'x'); put('C', 'c'); put('V', 'v');
             put('B', 'b'); put('N', 'n'); put('M', 'm'); put('"', '\''); put('|', '\\');
+        }};
+    }
+
+    private HashMap<Character, Character> createRussianShiftMap() {
+        logger.info("Was created russian Shift Map");
+        return new HashMap<>() {{
+            put('Ё', 'ё'); put('Й', 'й'); put('Ц', 'ц'); put('У', 'у'); put('К', 'к'); put('Е', 'е'); put('Н', 'н');
+            put('Г', 'г'); put('Ш', 'ш'); put('Щ', 'щ'); put('З', 'з'); put('Х', 'х'); put('Ъ', 'ъ'); put('Ф', 'ф');
+            put('Ы', 'ы'); put('В', 'в'); put('А', 'а'); put('П', 'п'); put('Р', 'р'); put('О', 'о'); put('Л', 'л');
+            put('Д', 'д'); put('Ж', 'ж'); put('Э', 'э'); put('Я', 'я'); put('Ч', 'ч'); put('С', 'с'); put('М', 'м');
+            put('И', 'и'); put('Т', 'т'); put('Ь', 'ь'); put('Б', 'б'); put('Ю', 'ю');
+        }};
+    }
+
+    private HashMap<Character, Character> createEnRuMap() {
+        logger.info("Was created EN/RU Map");
+        return new HashMap<>() {{
+            put('ё', '`'); put('й', 'q'); put('ц', 'w'); put('у', 'e'); put('к', 'r'); put('е', 't'); put('н', 'y');
+            put('г', 'u'); put('ш', 'i'); put('щ', 'o'); put('з', 'p'); put('х', '['); put('ъ', ']'); put('ф', 'a');
+            put('ы', 's'); put('в', 'd'); put('а', 'f'); put('п', 'g'); put('р', 'h'); put('о', 'j'); put('л', 'k');
+            put('д', 'l'); put('ж', ';'); put('э', '\''); put('я', 'z'); put('ч', 'x'); put('с', 'c'); put('м', 'v');
+            put('и', 'b'); put('т', 'n'); put('ь', 'm'); put('б', ','); put('ю', '.');
         }};
     }
 }
--- src/test/java/test/VirtKeyTests.java
+++ src/test/java/test/VirtKeyTests.java
@@ -7,7 +7,7 @@ import page.VirtKeyHomePage;
 
 public class VirtKeyTests extends test.CommonConditions {
     @Test (description = "Open 'Virtual Keyboard' page test")
-    public void t1_openVirtualKeyboardPage() {
+    public void t01_openKeyboardPage() {
 
         new VirtKeyHomePage(driver)
                 .openPage();
@@ -16,8 +16,8 @@ public class VirtKeyTests extends test.CommonConditions {
                 "URLs aren't equals!");
     }
 
-    @Test (description = "Show 'Virtual Keyboard' on the page")
-    public void t2_showVirtualKeyboardOnThePage() {
+    @Test (description = "Show 'Virtual Keyboard' on the page test")
+    public void t02_showKeyboard() {
 
         new VirtKeyHomePage(driver)
                 .openPage()
@@ -26,36 +26,131 @@ public class VirtKeyTests extends test.CommonConditions {
         Assert.assertNotNull(driver.findElement(By.className("keyboard")));
     }
 
-    @Test (description = "Write text with the virtual keyboard")
-    public void t3_writeTextWithTheVirtualKeyboardOnlyWithoutShift() {
+    @Test (description = "Maximize output textarea")
+    public void t03_maximizeOutputTextarea() {
+
+        new VirtKeyHomePage(driver)
+                .openPage()
+                .showKeyboard()
+                .maximizeTextArea();
+
+        Assert.assertEquals(driver
+                .findElement(By.className("use-keyboard-input")).getCssValue("height"),"450px");
+    }
+
+    @Test (description = "Write text without Shift in English test")
+    public void t04_writeTextWithoutShiftInEnglish() {
 
         String text = new VirtKeyHomePage(driver)
                 .openPage()
                 .showKeyboard()
+                .maximizeTextArea()
                 .writeText("hello ;-=`'123\\/.[]");
 
         Assert.assertEquals(text, "hello ;-=`'123\\/.[]");
     }
 
-    @Test (description = "Write text with the virtual keyboard")
-    public void t4_writeTextWithTheVirtualKeyboardOnlyWithShift() {
+    @Test (description = "Write text with Shift in English test")
+    public void t05_writeTextWithShiftInEnglish() {
 
         String text = new VirtKeyHomePage(driver)
                 .openPage()
                 .showKeyboard()
+                .maximizeTextArea()
                 .writeText("HELLO ~!@#$%^&*()_+{}|:\"<>?");
 
         Assert.assertEquals(text, "HELLO ~!@#$%^&*()_+{}|:\"<>?");
     }
 
-    @Test (description = "Write text with the virtual keyboard")
-    public void t5_writeTextWithTheVirtualKeyboardWithAndWithoutShift() {
+    @Test (description = "Write text with and without Shift in English test")
+    public void t06_writeTextWithAndWithoutShiftInEnglish() {
 
         String text = new VirtKeyHomePage(driver)
                 .openPage()
                 .showKeyboard()
+                .maximizeTextArea()
                 .writeText("HeLLo ~!@#$%her^&*()345_+{}|:\"<[]>?");
 
         Assert.assertEquals(text, "HeLLo ~!@#$%her^&*()345_+{}|:\"<[]>?");
+    }
+
+    @Test (description = "Write text without Shift in Russian test")
+    public void t07_writeTextWithoutShiftInRussian() {
+
+        String text = new VirtKeyHomePage(driver)
+                .openPage()
+                .showKeyboard()
+                .maximizeTextArea()
+                .writeText("ё1234567890-=йцуке нгшщзхъ\nфывапролджэ\\ячсмитьбю.");
+
+        Assert.assertEquals(text, "ё1234567890-=йцуке нгшщзхъ\nфывапролджэ\\ячсмитьбю.");
+    }
+
+    @Test (description = "Write text with Shift in Russian test")
+    public void t08_writeTextWithShiftInRussian() {
+
+        String text = new VirtKeyHomePage(driver)
+                .openPage()
+                .showKeyboard()
+                .maximizeTextArea()
+                .writeText("Ё!\"№;%:?*()_+ЙЦУКЕНГШЩЗХЪ\nФЫВА ПРОЛДЖЭ|ЯЧСМИТБЬЮ,");
+
+        Assert.assertEquals(text, "Ё!\"№;%:?*()_+ЙЦУКЕНГШЩЗХЪ\nФЫВА ПРОЛДЖЭ|ЯЧСМИТБЬЮ,");
+    }
+
+    @Test (description = "Write text with and without Shift in Russian test")
+    public void t09_writeTextWithAndWithoutShiftInRussian() {
+
+        String text = new VirtKeyHomePage(driver)
+                .openPage()
+                .showKeyboard()
+                .maximizeTextArea()
+                .writeText("Ё123;%:?*90_+ЙцукенГШЩЗХЪ\nФЫва ПРОлджЭ|ЯЧСмитБЬЮ,");
+
+        Assert.assertEquals(text, "Ё123;%:?*90_+ЙцукенГШЩЗХЪ\nФЫва ПРОлджЭ|ЯЧСмитБЬЮ,");
+    }
+
+    @Test (description = "Write text with and without Shift in Russian and English test")
+    public void t10_writeTextWithAndWithoutShiftInRussianAndEnglish() {
+
+        String text = new VirtKeyHomePage(driver)
+                .openPage()
+                .showKeyboard()
+                .maximizeTextArea()
+                .writeText("`123;%:?*90_+ЙцуrtyГШopХЪ\nФ{Ы}ва [] GHОлджЭ|ЯЧСм///bтБЬЮ,");
+
+        Assert.assertEquals(text, "`123;%:?*90_+ЙцуrtyГШopХЪ\nФ{Ы}ва [] GHОлджЭ|ЯЧСм///bтБЬЮ,");
+    }
+
+    @Test (description = "Write agile manifesto")
+    public void t11_writeAgileManifesto() {
+
+        String text = new VirtKeyHomePage(driver)
+                .openPage()
+                .showKeyboard()
+                .maximizeTextArea()
+                .writeText("We are uncovering better ways of developing\n" +
+                        "software by doing it and helping others do it.\n" +
+                        "Through this work we have come to value:\n" +
+                        "\n" +
+                        "Individuals and interactions over processes and tools\n" +
+                        "Working software over comprehensive documentation\n" +
+                        "Customer collaboration over contract negotiation\n" +
+                        "Responding to change over following a plan\n" +
+                        "\n" +
+                        "That is, while there is value in the items on\n" +
+                        "the right, we value the items on the left more.");
+
+        Assert.assertEquals(text, "We are uncovering better ways of developing\n" +
+                "software by doing it and helping others do it.\n" +
+                "Through this work we have come to value:\n" +
+                "\n" +
+                "Individuals and interactions over processes and tools\n" +
+                "Working software over comprehensive documentation\n" +
+                "Customer collaboration over contract negotiation\n" +
+                "Responding to change over following a plan\n" +
+                "\n" +
+                "That is, while there is value in the items on\n" +
+                "the right, we value the items on the left more.");
     }
 }
