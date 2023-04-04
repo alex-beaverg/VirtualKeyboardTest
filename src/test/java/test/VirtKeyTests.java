39de914760425377a527ff8b4467bb6d5c2ccb5d
Alexey Bobrikov <a.beaverg@gmail.com>
2023-04-05 02:46:57 +0300

    update

--- src/test/java/test/VirtKeyTests.java
+++ src/test/java/test/VirtKeyTests.java
@@ -118,9 +118,9 @@ public class VirtKeyTests extends test.CommonConditions {
                 .openPage()
                 .showKeyboard()
                 .maximizeTextArea()
-                .writeText("`123;%:?*90_+ЙцуrtyГШopХЪ\nФ{Ы}ва [] GHОлджЭ|ЯЧСм///bтБЬЮ,");
+                .writeText("`123;%:?*90_+Йцу1rtyГШopХЪ\nФ{Ы}ва [] GHОлджЭ|ЯЧСм///bтБЬЮ,");
 
-        Assert.assertEquals(text, "`123;%:?*90_+ЙцуrtyГШopХЪ\nФ{Ы}ва [] GHОлджЭ|ЯЧСм///bтБЬЮ,");
+        Assert.assertEquals(text, "`123;%:?*90_+Йцу1rtyГШopХЪ\nФ{Ы}ва [] GHОлджЭ|ЯЧСм///bтБЬЮ,");
     }
 
     @Test (description = "Write agile manifesto")
@@ -130,28 +130,30 @@ public class VirtKeyTests extends test.CommonConditions {
                 .openPage()
                 .showKeyboard()
                 .maximizeTextArea()
-                .writeText("We are uncovering better ways of developing\n" +
-                        "software by doing it and helping others do it.\n" +
-                        "Through this work we have come to value:\n" +
-                        "\n" +
-                        "Individuals and interactions over processes and tools\n" +
-                        "Working software over comprehensive documentation\n" +
-                        "Customer collaboration over contract negotiation\n" +
-                        "Responding to change over following a plan\n" +
-                        "\n" +
-                        "That is, while there is value in the items on\n" +
-                        "the right, we value the items on the left more.");
+                .writeText("""
+                        We are uncovering better ways of developing
+                        software by doing it and helping others do it.
+                        Through this work we have come to value:
 
-        Assert.assertEquals(text, "We are uncovering better ways of developing\n" +
-                "software by doing it and helping others do it.\n" +
-                "Through this work we have come to value:\n" +
-                "\n" +
-                "Individuals and interactions over processes and tools\n" +
-                "Working software over comprehensive documentation\n" +
-                "Customer collaboration over contract negotiation\n" +
-                "Responding to change over following a plan\n" +
-                "\n" +
-                "That is, while there is value in the items on\n" +
-                "the right, we value the items on the left more.");
+                        Individuals and interactions over processes and tools
+                        Working software over comprehensive documentation
+                        Customer collaboration over contract negotiation
+                        Responding to change over following a plan
+
+                        That is, while there is value in the items on
+                        the right, we value the items on the left more.""");
+
+        Assert.assertEquals(text, """
+                We are uncovering better ways of developing
+                software by doing it and helping others do it.
+                Through this work we have come to value:
+
+                Individuals and interactions over processes and tools
+                Working software over comprehensive documentation
+                Customer collaboration over contract negotiation
+                Responding to change over following a plan
+
+                That is, while there is value in the items on
+                the right, we value the items on the left more.""");
     }
 }
